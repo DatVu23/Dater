2abf45ee080e5e92b083f8445ca304fb923fe3b9
DatVuGcc <dat.vu@greencouchconsulting.com>
2019-06-10 22:31:27 +0300

    Updates

---
+++ precache-manifest.5cd71ef97ec403f09e58b7a1e832751c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba180c0eb8b54bd22eb6c7302e4c3b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f769c2cf9e72c96ae0b0",
+    "url": "/static/css/main.b4e41081.chunk.css"
+  },
+  {
+    "revision": "0a2ae096b342c4024a89",
+    "url": "/static/js/2.b8c4cbcf.chunk.js"
+  },
+  {
+    "revision": "f769c2cf9e72c96ae0b0",
+    "url": "/static/js/main.4a07de6d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
