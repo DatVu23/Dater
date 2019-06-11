c27582849fa08eb36d41503041be6ed0f890eccd
DatVuGcc <dat.vu@greencouchconsulting.com>
2019-06-11 21:34:38 +0300

    Updates

---
+++ precache-manifest.62c5b493de7c406678784dfebadb97e3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f6b75ff259d2471e4d3741a936aeff",
+    "url": "/dater/index.html"
+  },
+  {
+    "revision": "7613a0f67cb7942a2542",
+    "url": "/dater/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "1f3b68928a37b04e9653",
+    "url": "/dater/static/css/main.754038a1.chunk.css"
+  },
+  {
+    "revision": "7613a0f67cb7942a2542",
+    "url": "/dater/static/js/2.2ea83d1d.chunk.js"
+  },
+  {
+    "revision": "1f3b68928a37b04e9653",
+    "url": "/dater/static/js/main.cd7b2734.chunk.js"
+  },
+  {
+    "revision": "108fc9c7f4a8e184d4d4",
+    "url": "/dater/static/js/runtime~main.6c916518.js"
+  },
+  {
+    "revision": "8df0e5059539715ec213122db9b7bdfe",
+    "url": "/dater/static/media/Nietzsche.8df0e505.jpg"
+  },
+  {
+    "revision": "8df0e5059539715ec213122db9b7bdfe",
+    "url": "/dater/static/media/nietzsche.8df0e505.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/dater/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/dater/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/dater/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/dater/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
